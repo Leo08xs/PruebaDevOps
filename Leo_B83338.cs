c4b131aa65acfb0c86361c15126f126977a19627
Leo08xs <91703231+Leo08xs@users.noreply.github.com>
2022-06-03 12:43:01 -0600

    Create Leo_B83338.cs

---
+++ Leo_B83338.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PruebaDevOps
+{
+    // Leonardo
+    // B83338
+    class Main
+    {
+        public String Mensaje()
+        {
+            String mensaje = "Marcell rico";
+            return mensaje;
+        }
+    }
+}
